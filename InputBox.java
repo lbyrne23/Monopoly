034551b4172c97cd859e64b5f09f710c52c4fd1f
Fiachra Dunn <fiachradunn3@gmail.com>
2017-02-04 15:03:37 +0000

    And so it begins.

---
+++ InputBox.java
@@ -0,0 +1,12 @@
+// A Class to take input from a text-box in a java frame and store it for future use.
+// Team : Cessna Skyhawk
+// Michael Jordan
+// Lucy Byrne
+// Fiachra Dunn
+//
+import javax.swing.JFrame;
+
+class public InputBox{
+	
+}
+
