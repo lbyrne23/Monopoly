a5020c38da0513b3b3f888f68d529884071955d0
MJ <michaeljordan2357@gmail.com>
2017-02-12 20:47:19 +0000

    :(

--- src/packA/showTable.java
+++ src/packA/showTable.java
@@ -1,22 +1,18 @@
 package packA;
 
-import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import javax.swing.Timer;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-import javax.imageio.ImageIO;
-//import java.io.InputStreamReader;
-//import java.io.BufferedReader;
-
+import javax.swing.Timer;
 import javax.swing.JPanel;
-//import javax.swing.JTextField;
-//import javax.swing.JFrame;
+
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.util.ArrayList;
+
 
 import packA.Player;
 
@@ -37,34 +33,30 @@ public class showTable extends JPanel {
 		{
 			playerList.add(new Player(i));
 		}
-
+		
+		int i = 0;
 		for (Player A:playerList){
-			int i = 0;
 			A.setLocation(i);
 			i++;
 		}
 	}
 
-
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 		g.drawImage( image, 0, 0, 700, 704, null);
-
-		for (Player A:playerList)
-		{
-			A.paintComponent(g);
-		}
+		playerList.get(0).paintComponent(g);
+		repaint();
 	}
 
 	public void sprint1Test(){
-		Timer timer = new Timer(500, new ActionListener(){
+		Timer timer = new Timer(100, new ActionListener(){
 			public void actionPerformed(ActionEvent e){
-
 				for(Player A:playerList)
 				{
 					A.setLocation( (A.getPosition() + 1) %39);
+					repaint();
 				}
-				repaint();
+//				remove(0);
 			}		
 		});
 		timer.start();
