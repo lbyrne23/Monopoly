79c2b04acadafec011107a0d610a0bd985a228bb
MJ <michaeljordan2357@gmail.com>
2017-02-18 20:32:29 +0000

    Houses For Sale

--- src/packA/Property.java
+++ src/packA/Property.java
@@ -6,27 +6,14 @@ public class Property {
 	private String name;
 	private int owner;
 	private int price;
-	private int rent;											// Using a fixed rent for Sprint 2.
-	private int mortgage;
-
-//	private String colour;
-//	
-//	private int houseCost;
-//	private int oneHouse;
-//	private int twoHouse;
-//	private int threeHouse;
-//	private int fourHouse;
-//	
-//	private int hotelCost;
-//	private int hotel;
-		
+	private int rent;	
 	
-	public void property(String nameIN, int priceIN, int mortgageIN){
+	
+	public Property(String nameIN, int ownerIN, int priceIN, int rentIN){
 		name = nameIN;
-		owner = -1;
+		owner = ownerIN;
 		price = priceIN;
-		rent = 50;
-		mortgage = mortgageIN;
+		rent = rentIN;
 	}
 	
 	public String returnName(){
@@ -49,46 +36,36 @@ public class Property {
 	public int returnRent(){
 		return rent;
 	}
-	
-	public int returnMortgage(){
-		return mortgage;
-	}
-	
+		
 	public void boughtProperty(int playerNum){
 		owner = playerNum;
 	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-//	public void propertyAdd() {
-//		propertyList.add("Old Kent Road");
-//		propertyList.add("Whitechapel Rd");
-//		propertyList.add("The Angel Islington");
-//		propertyList.add("Euston Rd");
-//		propertyList.add("Pentonville Rd");
-//		propertyList.add("Pall Mall");
-//		propertyList.add("Whitehall");
-//		propertyList.add("Northumberland Ave");
-//		propertyList.add("Bow St");
-//		propertyList.add("Marlborough St");
-//		propertyList.add("Vine St");
-//		propertyList.add("Strand");
-//		propertyList.add("Fleet St");
-//		propertyList.add("Trafalgar Sq");
-//		propertyList.add("Leicester Sq");
-//		propertyList.add("Coventry St");
-//		propertyList.add("Picadilly");
-//		propertyList.add("Regent St");
-//		propertyList.add("Oxford St");
-//		propertyList.add("Bond St");
-//		propertyList.add("Park Lane");
-//		propertyList.add("Mayfair");
-//	}
+	public void propertyAdd() {
+		ArrayList<Property> propertyList = new ArrayList<Property>();
+		
+		// Owner initially set to -1 when there is no owner. Rent set to the constant 10 for Sprint 2.
+		propertyList.add(new Property("Old Kent Rd", -1, 60, 10));
+		propertyList.add(new Property("Whitechapel Rd", -1, 60, 10));
+		propertyList.add(new Property("The Angel Islington", -1, 100, 10));
+		propertyList.add(new Property("Euston Rd", -1, 100, 10));
+		propertyList.add(new Property("Pentonville Rd", -1, 120, 10));
+		propertyList.add(new Property("Pall Mall", -1, 140, 10));
+		propertyList.add(new Property("Whitehall", -1, 140, 10));
+		propertyList.add(new Property("Northumberland Ave", -1, 160, 10));
+		propertyList.add(new Property("Bow St", -1, 180, 10));
+		propertyList.add(new Property("Marlborough St", -1, 180, 10));
+		propertyList.add(new Property("Vine St", -1, 200, 10));
+		propertyList.add(new Property("Strand", -1, 220, 10));
+		propertyList.add(new Property("Fleet St", -1, 220, 10));
+		propertyList.add(new Property("Trafalgar Sq", -1, 240, 10));
+		propertyList.add(new Property("Leicester Sq", -1, 260, 10));
+		propertyList.add(new Property("Coventry St", -1, 260, 10));
+		propertyList.add(new Property("Picadilly", -1, 280, 10));
+		propertyList.add(new Property("Regent St", -1, 300, 10));
+		propertyList.add(new Property("Oxford St", -1, 300, 10));
+		propertyList.add(new Property("Bond St", -1, 320, 10));
+		propertyList.add(new Property("Park Lane", -1, 350, 10));
+		propertyList.add(new Property("Mayfair", -1, 400, 10));
+	}
 }
