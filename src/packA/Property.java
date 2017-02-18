e021b516090e9ceba29d21647151263ee6c46cc0
MJ <michaeljordan2357@gmail.com>
2017-02-18 18:25:58 +0000

    Property Class

---
+++ src/packA/Property.java
@@ -0,0 +1,32 @@
+package packA;
+
+import java.util.ArrayList;
+
+public class Property {
+	static ArrayList<String> propertyList = new ArrayList<String>();
+	
+	public void propertyAdd() {
+		propertyList.add("Old Kent Road");
+		propertyList.add("Whitechapel Rd");
+		propertyList.add("The Angel Islington");
+		propertyList.add("Euston Rd");
+		propertyList.add("Pentonville Rd");
+		propertyList.add("Pall Mall");
+		propertyList.add("Whitehall");
+		propertyList.add("Northumberland Ave");
+		propertyList.add("Bow St");
+		propertyList.add("Marlborough St");
+		propertyList.add("Vine St");
+		propertyList.add("Strand");
+		propertyList.add("Fleet St");
+		propertyList.add("Trafalgar Sq");
+		propertyList.add("Leicester Sq");
+		propertyList.add("Coventry St");
+		propertyList.add("Picadilly");
+		propertyList.add("Regent St");
+		propertyList.add("Oxford St");
+		propertyList.add("Bond St");
+		propertyList.add("Park Lane");
+		propertyList.add("Mayfair");
+	}
+}
