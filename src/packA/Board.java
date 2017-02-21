6314f20e09726863771f8829b20d0dcbdfa9fb9f
Lucy Byrne <lucy.byrne@ucdconnect.ie>
2017-02-21 15:43:08 +0000

    I hope i didnt dun goof

--- src/packA/Board.java
+++ src/packA/Board.java
@@ -21,13 +21,14 @@ import packA.Player;
 
 public class Board extends JPanel {
 	private BufferedImage  image = null;	
-	private ArrayList<Player> playerList = new ArrayList<Player>(6);// Array list to store players.
-	private PropertyList properties = new PropertyList();
-	private int numberOfPlayers;
-	private JTextArea output;
-	
+	private static ArrayList<Player> playerList = new ArrayList<Player>(6);// Array list to store players.
+	//private PropertyList properties = new PropertyList();
+	private static int numberOfPlayers;
+	private static JTextArea output;
+	static int winner;
+
 	//Following integers are for tracking details of players' turns. 
-	private int playerTurn;
+	private static int playerTurn;
 
 
 	public Board(int players, JTextArea newOutput) {
@@ -54,13 +55,13 @@ public class Board extends JPanel {
 			p.paintComponent(g);
 		}
 	}
-	
-	public String squareInfo(){
+
+	/*public String squareInfo(){
 		//Get property at location of current players turn.
 		Player tmpPlayer = playerList.get(playerTurn);
 		Property tmpProperty = properties.get(tmpPlayer.getPosition());
 		String info;
-		
+
 		if(tmpProperty.returnOwner() == null){
 			info = tmpProperty.returnName() + "\n";
 		}
@@ -68,18 +69,18 @@ public class Board extends JPanel {
 		info = tmpProperty.returnName() + " ; \n-This property is on the market for £"
 		+ tmpProperty.returnPrice() + "\n-It has rent of £" + tmpProperty.returnRent() + ".\n";
 		}
-		
+
 		else if(tmpProperty.returnOwner() == playerTurn){
 			info = "This is your property!\n";
 		}
-		
+
 		else{
 			info = tmpProperty.returnName() + " ; \n -Player " + tmpProperty.returnOwner()
 			+ " owns this property.\n You must pay rent of £" + tmpProperty.returnRent() + ".\n";
 		}
-		
+
 		return info;
-	}
+	}*/
 
 
 	public void playerAction(String command){
@@ -94,30 +95,11 @@ public class Board extends JPanel {
 				playerTurn++;
 				output.append("Roll to see who goes first\n");
 
-				/* All below is dice roll algorithm - will clean up into a method soon
-				int Roll;
-				int[] firstRolls = new int[numberOfPlayers];
+				goFirst();
 
-				for(int i = 0; i < numberOfPlayers; i++){
-					Roll = Dice.Roll();
-					playerList.get(i).setFirstRoll(Roll);
-					firstRolls[i] = Roll;
-					output.append(playerList.get(i).getName() + " : " + playerList.get(i).getFirstRoll() + "\n");
-				}
-
-				Arrays.sort(firstRolls);
-
-				for(int count = 0; count < numberOfPlayers; count++){
-					if(playerList.get(count).getFirstRoll() == firstRolls[numberOfPlayers]){
-						//Set this player to roll first
-					}
-				}
-
-				Dice.rollAgain = false;
-				End of dice roll shite*/
-				output.append("(Player who rolled highest) goes first.\nEnter 'roll' \n");
+				output.append(playerList.get(playerTurn).getName() + " goes first.\nEnter 'roll' \n");
 				return;
-				
+
 			}
 
 
@@ -138,15 +120,15 @@ public class Board extends JPanel {
 			}
 		}
 
-		
-		if(command.equalsIgnoreCase("buy")){
+
+		/*if(command.equalsIgnoreCase("buy")){
 			buyFunction();
 		}
-		
+
 		else if(command.equalsIgnoreCase("property")){
 			propertyFunction();
-		}
-		
+		}*/
+
 		else if(command.equalsIgnoreCase("done")){
 			doneFunction();
 		}
@@ -154,25 +136,30 @@ public class Board extends JPanel {
 		else if(command.equalsIgnoreCase("roll")){
 			rollFunction();
 		}
-		
+
 		else if(command.equalsIgnoreCase("help")){
-		helpFunction();
+			helpFunction();
 		}
 
 		else if(command.equalsIgnoreCase("balance")){
 			output.append("\nYour balance: " + playerList.get(playerTurn).getBalance() + "\n");
+		}
+		
+		else if(command.equalsIgnoreCase("quit")){
+			highestPlayer();
+			output.append("\nGame Over. Winner is " + playerList.get(winner).getName() + " with £" + playerList.get(winner).getBalance() + "\n");
 		}
 
 		else {
 			output.append("\nInvalid command, enter 'help' for a list of commands. \n");
 		}
 	}
-	
+
 	public void rollFunction(){
 		if(Dice.allowedRoll == 0 || Dice.allowedRoll == 2){
 			Player tmpPlayer = playerList.get(playerTurn);
 			int thisRoll = Dice.Roll();
-			
+
 			if((tmpPlayer.getPosition()+ thisRoll)%40 < tmpPlayer.getPosition()){
 				tmpPlayer.updateBalance(200);
 			}
@@ -181,8 +168,8 @@ public class Board extends JPanel {
 			output.append("\n"+ Dice.words() + "\n");
 			repaint();
 
-			output.append(squareInfo());
-		
+			//output.append(squareInfo());
+
 			if(Dice.allowedRoll == 2){
 				output.append("\nYou are able to roll again!\n");
 			}
@@ -190,21 +177,9 @@ public class Board extends JPanel {
 		else{
 			output.append("\nYou cannot roll again.");
 		}
-		
 
-		/*if(	Dice.rollAgain == true){
-			output.append("As you rolled doubles you can roll again \n"
-					+ "Type 'again' to do so\n");
-
-			output.append("Doubles: Roll again\n");
-			tmpPlayer.setLocation((tmpPlayer.getPosition()+Dice.Roll())%40);
-			output.append(Dice.words());
-
-
-			Dice.rollAgain = false; 
-		}*/
 	}
-	
+
 	public void helpFunction(){
 		output.append("\n'roll' : Roll dice \n"
 				+ "'pay rent' : Pay rent of square you landed on \n"
@@ -214,27 +189,27 @@ public class Board extends JPanel {
 				+ "'done' : Finish your turn \n"
 				+ "'quit' : Quit the game \n");
 	}
-	
+
 	public void doneFunction(){
 		if(Dice.allowedRoll != 0 && Dice.allowedRoll != 2){
-		Dice.allowedRoll = 0;
-		playerTurn = (playerTurn+1)%numberOfPlayers;
-		output.append("\n" + playerList.get(playerTurn).getName() +"'s turn. Roll.\n");
+			Dice.allowedRoll = 0;
+			playerTurn = (playerTurn+1)%numberOfPlayers;
+			output.append("\n" + playerList.get(playerTurn).getName() +"'s turn. Roll.\n");
 		}
 		else{
 			output.append("\nYou cannot end your turn without rolling");
 		}
-		
-		
+
+
 	}
-	
-	public void buyFunction(){
+
+	/* public void buyFunction(){
 		Player currPlayer = playerList.get(playerTurn);
 		Property currProperty = properties.get(currPlayer.getPosition());
 		if(currProperty.returnOwner() == null || currProperty.returnOwner() >= 0){ 		// If property is owned or can't be purchased
 			output.append("\n This property cannot be purchased. \n");
 		}
-		
+
 		else if (currPlayer.getBalance() >= currProperty.returnPrice()){				//If player can afford property, purchase
 			currProperty.setOwner(playerTurn);											//set Owner using whose turn it is.
 			currPlayer.updateBalance(-(currProperty.returnPrice()));					//subtract cost from player Balance
@@ -244,12 +219,58 @@ public class Board extends JPanel {
 			output.append("\nYou cannot afford this property.\n");
 		}
 	}
-	
+
 	public void propertyFunction(){
 			output.append("\nThe properties you own are as follow;\n");
 		for(Property p : properties){
 			if(p.returnOwner() != null && p.returnOwner() == playerTurn){
 				output.append("\n" + p.returnName() + " : \n-The current rent is £" + p.returnRent() +"\n");
+			}
+		}
+	} */
+
+
+	//Function to determine who goes first
+	public static void goFirst(){
+		int Roll;
+		int[] firstRolls = new int[numberOfPlayers]; //array to store the first roll
+
+		for(int i = 0; i < numberOfPlayers; i++){
+			Roll = Dice.Roll();
+			playerList.get(i).setFirstRoll(Roll); //set value of first roll
+			firstRolls[i] = Roll;
+			output.append(playerList.get(i).getName() + " : " + playerList.get(i).getFirstRoll() + "\n"); //print first rolls to screen
+		}
+
+		//sort the first rolls from lowest to highest
+		//this means the highest value is the last value in the array
+		//meaning whoever rolled this highest value goes first
+		Arrays.sort(firstRolls);
+
+		//go through list and determine which player's value matched the highest value
+		for(int count = 0; count < numberOfPlayers; count++){ 
+			if(playerList.get(count).getFirstRoll() == firstRolls[numberOfPlayers-1]){
+				playerTurn = count;
+			}
+		}
+		Dice.allowedRoll = 0;
+	}
+	
+	public static void highestPlayer(){
+		int[] balances = new int[numberOfPlayers]; //array to store the balances
+		
+		for(int i = 0; i < numberOfPlayers; i++){
+			balances[i] = playerList.get(i).getBalance();
+			output.append(playerList.get(i).getName() + " : £" + playerList.get(i).getBalance() + "\n"); //print balances to screen
+		}
+
+		//sort balances from lowest to highest
+		Arrays.sort(balances);
+
+		//go through list and determine which player's value matched the highest value
+		for(int count = 0; count < numberOfPlayers; count++){ 
+			if(playerList.get(count).getBalance() == balances[numberOfPlayers-1]){
+				winner = count;
 			}
 		}
 	}
