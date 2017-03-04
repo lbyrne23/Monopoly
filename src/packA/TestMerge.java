fecbf8e66874bbaa7cebd0d3f3ac9ae1b8d10406
fiachra Dunn <fiachra Dunn>
2017-03-04 14:52:47 +0000

    Oh golly duude

--- src/packA/TestMerge.java
+++ src/packA/TestMerge.java
@@ -3,4 +3,13 @@ package packA;
 public class TestMerge {
 	//This is a test file.
 	//MERGE ME.
+	public void foobar(){
+		String string = "Hello Captain ;)";
+		string.endsWith("harikiri");
+		for(int i=0; i<3; i++){
+			//skeet skeet
+		}
+	}
 }
+
+
