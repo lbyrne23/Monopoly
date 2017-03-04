052170ae8150e0ba52c24c5f7c9e15680500ad15
MJ <michaeljordan2357@gmail.com>
2017-03-04 14:54:06 +0000

    golly gosh can this merge?

--- src/packA/TestMerge.java
+++ src/packA/TestMerge.java
@@ -3,4 +3,12 @@ package packA;
 public class TestMerge {
 	//This is a test file.
 	//MERGE ME.
+	
+	public void mj (){
+		int i = 0;
+		while (i = 0; i < 100; i++)
+		{
+			output.append("MJ\n");
+		}
+	}
 }
