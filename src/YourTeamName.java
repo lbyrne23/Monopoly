0bd2e3fa97195240f10d5433e15d2be2a0781b7e
Lucy Byrne <lucy.byrne@ucdconnect.ie>
2017-04-22 15:32:52 +0100

    yourteamname update

--- src/YourTeamName.java
+++ src/YourTeamName.java
@@ -1,23 +1,60 @@
+//Team : Cessna Skyhawk
+//Michael Jordan
+//Lucy Byrne
+//Fiachra Dunn
+//
+//
 
-public class YourTeamName implements Bot {
+
+
+public class CessnaSkyhawk implements Bot {
 	
 	// The public API of YourTeamName must not change
 	// You cannot change any other classes
 	// YourTeamName may not alter the state of the board or the player objects
 	// It may only inspect the state of the board and the player objects
-
+	private static int i = 0;
 	
-	YourTeamName (BoardAPI board, PlayerAPI player, DiceAPI dice) {
+	CessnaSkyhawk (BoardAPI board, PlayerAPI player, DiceAPI dice) {
+		System.out.println("asdasd");
 		return;
 	}
 	
 	public String getName () {
-		return "YourTeamName";
+		return "CessnaSkyhawk";
 	}
 	
 	public String getCommand () {
-		// Add your code here
-		return "help";
+		switch (i){
+		case 0 : 
+			return "roll";
+		case 1 :
+			return "done";
+		case 2 : 
+			return "pay rent";
+		case 3 :
+			return "build";
+		case 4 :
+			return "demolish";
+		case 5 :
+			return "mortgage";
+		case 6 :
+			return "redeem";
+		case 7 : 
+			return "bankrupt";
+		case 8 :
+			return "property";
+		case 9 : 
+			return "balance";
+		case 10 :
+			return "quit";
+			
+		default : 
+			i = 1;
+			return "roll";
+		}
+
+
 	}
 	
 	public String getDecision () {
